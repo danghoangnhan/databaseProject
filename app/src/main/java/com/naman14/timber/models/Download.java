d1090a9b35f2c2647f726e1b4c451cf06dcd0a06
Andychentwfju <75734147+Andychentwfju@users.noreply.github.com>
2021-01-04 00:33:04 +0800

    clean

--- app/src/main/java/com/naman14/timber/models/Download.java
+++
@@ -1,8 +0,0 @@
-package com.naman14.timber.models;
-import com.google.gson.annotations.Expose;
-import com.google.gson.annotations.SerializedName;
-public class Download {
-    @SerializedName("tuneId")
-    private Integer tuneId;
-
-}
